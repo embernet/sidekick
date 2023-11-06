60b245a54a37cded8f632acac85e0a9ca039d6a3
Karim Tabet <karim@tabet.uk>
2023-11-06 12:31:50 +0000

    Move database creation out to separate file

--- server/app.py
+++ server/app.py
@@ -1,13 +1,10 @@
 import os
-import json
 import logging
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
-from flask_migrate import Migrate, upgrade
+from flask_migrate import Migrate
 from flask_cors import CORS
-from sqlalchemy_utils import database_exists
-from sqlalchemy.exc import NoResultFound
 from flask_oidc import OpenIDConnect
 
 app = Flask(__name__)
@@ -25,46 +22,5 @@ jwt = JWTManager(app)
 CORS(app)
 migrate = Migrate(app, db)
 # oidc = OpenIDConnect(app)
-
-if not database_exists(app.config["SQLALCHEMY_DATABASE_URI"]):
-    app.logger.info("Database doesn't exist, attempting to create one now.")
-    with app.app_context():
-        db.create_all()
-        upgrade(directory="migrations")
-        app.logger.info("Created database "
-                        f"{app.config['SQLALCHEMY_DATABASE_URI']}")
-
-from utils import DBUtils, get_random_string
-
-with app.app_context():
-    # Create sidekick user they don't exist
-    try:
-        DBUtils.get_user("sidekick")
-    except NoResultFound:
-        DBUtils.create_user(user_id="sidekick",
-                            password=get_random_string())
-
-    # Create admin user if they don't exist
-    try:
-        DBUtils.get_user("admin")
-    except NoResultFound:
-        DBUtils.create_user(user_id="admin",
-                            password="changemenow",
-                            properties={"roles": {"admin": True}})
-
-    # Create system settings documents if they don't exist
-    for settings_file in os.listdir("system_settings"):
-        settings_name = settings_file.split(".")[0]
-        try:
-            DBUtils.get_document(user_id="sidekick",
-                                 name=settings_name,
-                                 type="system_settings")
-        except NoResultFound:
-            settings = json.loads(open("system_settings/"
-                                       f"{settings_file}").read())
-            DBUtils.create_document(user_id="sidekick",
-                                    name=settings_name,
-                                    type="system_settings",
-                                    content=settings)
 
 import routes
