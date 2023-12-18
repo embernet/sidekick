cd682baaca552024df267e76914e045a455e05d7
embernet <embernet1@gmail.com>
2023-12-18 10:57:33 +0000

    updated init.py to be importable from another python module and added error logging, updated run.py to run init() before starting

--- server/init.py
+++ server/init.py
@@ -4,31 +4,46 @@ from sqlalchemy.engine.url import make_url
 from sqlalchemy.exc import NoResultFound
 from app import app, db
 
-db_url = make_url(os.environ["SQLALCHEMY_DATABASE_URI"])
-db_dialect_name = db_url.get_dialect().name
-
-with app.app_context():
-    from utils import DBUtils, get_random_string, update_system_settings, update_default_settings
-    
-    if db_dialect_name == "sqlite":
-        db.create_all()
-    if db_dialect_name == "postgresql":
-        upgrade(directory="migrations")
-
-    # Create sidekick user if they don't exist
+def init():
     try:
-        DBUtils.get_user("sidekick")
-    except NoResultFound:
-        DBUtils.create_user(user_id="sidekick",
-                            password=get_random_string())
+        app.logger.info("Initializing Sidekick server...")
+        db_url = make_url(os.environ["SQLALCHEMY_DATABASE_URI"])
+        db_dialect_name = db_url.get_dialect().name
 
-    # Create admin user if they don't exist
-    try:
-        DBUtils.get_user("admin")
-    except NoResultFound:
-        DBUtils.create_user(user_id="admin",
-                            password="changemenow",
-                            properties={"roles": {"admin": True}})
-        
-    update_system_settings()
-    update_default_settings("sidekick")
+        with app.app_context():
+            from utils import DBUtils, get_random_string, update_system_settings, update_default_settings
+            
+            if db_dialect_name == "sqlite":
+                db.create_all()
+            if db_dialect_name == "postgresql":
+                upgrade(directory="migrations")
+
+            # Create sidekick user if they don't exist
+            try:
+                DBUtils.get_user("sidekick")
+            except NoResultFound:
+                app.logger.info("Creating sidekick user...")
+                DBUtils.create_user(user_id="sidekick",
+                                    password=get_random_string())
+                app.logger.info("Sidekick user created.")
+
+            # Create admin user if they don't exist
+            try:
+                DBUtils.get_user("admin")
+            except NoResultFound:
+                app.logger.info("Creating admin user...")
+                DBUtils.create_user(user_id="admin",
+                                    password="changemenow",
+                                    properties={"roles": {"admin": True}})
+                app.logger.info("Admin user created.")
+                
+            update_system_settings()
+            update_default_settings("sidekick")
+        app.logger.info("Sidekick server initialized.")
+    except Exception as e:
+        app.logger.error("Sidekick server initialization failed.")
+        app.logger.error(e)
+        raise e
+
+if __name__ == '__main__':
+    init()
--- server/run.py
+++ server/run.py
@@ -1,5 +1,7 @@
 import os
 from app import app
+from init import init
 
 if __name__ == '__main__':
+    init()
     app.run(port=os.environ.get("SIDEKICK_SERVER_PORT", 5000))
