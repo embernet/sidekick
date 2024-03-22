9ef04a06be8d8fb431e8446fa472291af3e551ee
embernet <embernet1@gmail.com>
2024-03-22 11:00:17 +0000

    updated version to 0.3

--- web_ui/src/App.js
+++ web_ui/src/App.js
@@ -58,7 +58,7 @@ import Admin from './Admin';
 import SidekickAI from './SidekickAI';
 import StatusBar from './StatusBar';
 
-const VERSION = "0.2.4";
+const VERSION = "0.3";
 
 const ScriptIcon = SmartDisplayOutlinedIcon;
 const ScriptsExplorerIcon = SubscriptionsOutlinedIcon;
