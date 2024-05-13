6afa1a303b61cba3ccb742c80bcbaa4d6948f154
Karim Tabet <karim@tabet.uk>
2024-05-13 13:50:22 +0100

    Optionally set open ai base URL as env var

--- server/app.py
+++ server/app.py
@@ -20,6 +20,7 @@ app.config["JWT_SECRET_KEY"] = os.environ["JWT_SECRET_KEY"]
 app.config["JWT_ACCESS_TOKEN_EXPIRES"] = False
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ["SQLALCHEMY_DATABASE_URI"]
 app.config["OPENAI_API_KEY"] = os.environ["OPENAI_API_KEY"]
+app.config["OPENAI_BASE_URL"] = os.environ.get("OPENAI_BASE_URL", "https://api.openai.com/v1")
 app.config["OPENAI_PROXY"] = os.environ.get("OPENAI_PROXY")
 
 # Optionallay count chat tokens if specified in the env var
